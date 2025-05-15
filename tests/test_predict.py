66016deee6c9da9e73e3dae157d6f4ab9eb1a0d9
chrisdvkta <chrisdevkota77@gmail.com>
2025-05-16 02:55:12 +0545

    fix: prediction test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -6,7 +6,6 @@ client = TestClient(app)
 
 
 def test_prediction():
-    # training for creating a model first.
     training_csv = """age,blood_pressure,cholestrol,smoker,risk
 45,120,200,No,Low
 52,135,240,Yes,High
@@ -16,23 +15,21 @@ def test_prediction():
 
     train_file = BytesIO(training_csv.encode("utf-8"))
     response_train = client.post(
-        "/train", {"file": ("training_data.csv", train_file, "text/csv")}
+        "/train", files={"file": ("train.csv", train_file, "text/csv")}
     )
-
     assert response_train.status_code == 200
-    # predict without the label column
-    csv_data = """
-age,blood_pressure,cholesterol,smoker
-    45,120,200,No
+
+    # without a label
+    csv_data = """age,blood_pressure,cholestrol,smoker
+45,120,200,No
 52,135,240,Yes
 60,140,260,No
-    """
-
-    file = BytesIO(csv_data.encode("utf-8"))
-
+"""
+    predict_file = BytesIO(csv_data.encode("utf-8"))
     response = client.post(
-        "/predict", files={"file": ("predict.csv", file, "text/csv")}
+        "/predict", files={"file": ("predict.csv", predict_file, "text/csv")}
     )
+
     assert response.status_code == 200
     json_data = response.json()
     assert "predictions" in json_data
