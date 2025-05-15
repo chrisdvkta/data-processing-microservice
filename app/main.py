138cee6418bc7882f4b92b52d291529ce87a1724
chrisdvkta <chrisdevkota77@gmail.com>
2025-05-15 23:27:24 +0545

    add: predict route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from fastapi import FastAPI, File, HTTPException, UploadFile
 import pandas as pd
+from app.predict import predict_from_df
 from app.train import train_model_df
 
 
@@ -23,5 +24,24 @@ async def train_model(file: UploadFile = File(...)):
 
         metrics = train_model_df(df)
         return {"message": "model trained successfully", "metrics": metrics}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Only CSV files are supported")
+
+    try:
+        contents = await file.read()
+        df = pd.read_csv(StringIO(contents.decode("utf-8")))
+        predictions = predict_from_df(df)
+        return {"predictions": predictions}
+
+    except FileNotFoundError:
+        raise HTTPException(
+            status_code=404, detail="no trained model found. train a model first"
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
